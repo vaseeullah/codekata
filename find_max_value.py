d3dc1e021f7dc136dc2963a3f80322e9c1ac2c83
vaseeullah <47440496+vaseeullah@users.noreply.github.com>
2019-02-21 09:01:51 +0530

    Update find_max_value.py

--- find_max_value.py
+++ find_max_value.py
@@ -1,6 +1,6 @@
 z=int(input())
-a=[]
-a=input().split()
-for i in range(0,len(a)):
-    a[i]=int(a[i])
-print(min(a))
+c=[]
+c=input().split()
+for i in range(0,len(c)):
+    c[i]=int(c[i])
+print(min(c))
